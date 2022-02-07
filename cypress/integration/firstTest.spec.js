d006dc3df46af41bcf1be6e2830a20c500745ba0
vitalik991720 <vitalik9917@rambler.ru>
2022-02-07 11:59:59 +0200

    new branch test

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -4,9 +4,10 @@
 //     it("Show erro: Minimum amount of the  1 UAH" , ()=> {});
 //    });
 // });
+//it.only('Using Get with Find and Eq', ()=>{
+//  cy.visit("https://next.privat24.ua")
+//   cy.get('*[class^="card"]')
+//});
+
 /// <reference types="Cypress" />
 
-it.only('Using Get with Find and Eq', ()=>{
-   cy.visit("https://next.privat24.ua")
-   cy.get('*[class^="card"]')
-});
