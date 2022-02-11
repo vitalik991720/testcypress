9170719db802f1dde9a5eef99ccdcdeeb19f1e9a
vitalik991720 <vitalik9917@rambler.ru>
2022-02-11 10:03:19 +0200

    web element lessons update

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -13,44 +13,65 @@
 
 /// <reference types="Cypress" />
 
-it('SHOULD', ()=>{
+// it('SHOULD', ()=>{
+//    cy.visit('https://next.privat24.ua/mobile?lang=en')
+//    cy.get('[data-qa-node="amount"]')
+//    .type(100)
+//    .should('have.value', 100)
+//    .and('be.visible')
+// })
+
+// it('EXPECT', ()=>{
+//    cy.visit('https://next.privat24.ua/mobile?lang=en')
+//    cy.get('[data-qa-node="amount"]')
+//    .type(100).then( input=>{
+//       expect(input).to.have.value(100)
+//    })
+// })
+// it('сheck default value for Deposits', ()=>{
+//    cy.visit('https://next.privat24.ua/deposit?lang=en')
+//    cy.get('[data-qa-value="UAH"]')
+//    .should('be.checked')
+// })
+
+// it('сheck is visible Archive ', ()=>{
+//    cy.visit('https://next.privat24.ua/deposit?lang=en')
+//    cy.contains('Мої депозити')
+//    .trigger('mouseover')
+//    .get('#archiveDeposits')
+//    .should('be.visible') 
+// })
+
+
+// it('chech is correct attr in button', ()=>{
+//    cy.visit('https://next.privat24.ua?lang=en')
+//    cy.contains('Show cards')
+//    .should('have.attr', 'type') 
+//    .and('match', /button/)
+// })
+// it.only('сheck default value for Deposits', ()=>{
+//    cy.visit('https://next.privat24.ua?lang=en')
+//    cy.url()
+//    .should('eq', 'https://next.privat24.ua/?lang=en')
+// })
+
+
+//type
+it('type',()=>{
    cy.visit('https://next.privat24.ua/mobile?lang=en')
-   cy.get('[data-qa-node="amount"]')
-   .type(100)
-   .should('have.value', 100)
-   .and('be.visible')
+   .get('[data-qa-node="phone-number"]')
+   .type(123414 )
 })
-
-it('EXPECT', ()=>{
+//focus
+it('focus',()=>{
    cy.visit('https://next.privat24.ua/mobile?lang=en')
-   cy.get('[data-qa-node="amount"]')
-   .type(100).then( input=>{
-      expect(input).to.have.value(100)
-   })
+   .get('[data-qa-node="amount"]')
+   .focus()
 })
-it('сheck default value for Deposits', ()=>{
-   cy.visit('https://next.privat24.ua/deposit?lang=en')
-   cy.get('[data-qa-value="UAH"]')
-   .should('be.checked')
-})
-
-it('сheck is visible Archive ', ()=>{
-   cy.visit('https://next.privat24.ua/deposit?lang=en')
-   cy.contains('Мої депозити')
-   .trigger('mouseover')
-   .get('#archiveDeposits')
-   .should('be.visible') 
-})
-
-
-it('chech is correct attr in button', ()=>{
-   cy.visit('https://next.privat24.ua?lang=en')
-   cy.contains('Show cards')
-   .should('have.attr', 'type') 
-   .and('match', /button/)
-})
-it.only('сheck default value for Deposits', ()=>{
-   cy.visit('https://next.privat24.ua?lang=en')
-   cy.url()
-   .should('eq', 'https://next.privat24.ua/?lang=en')
+//blur
+it.only('blur',()=>{
+   cy.visit('https://next.privat24.ua/mobile?lang=en')
+   .get('[data-qa-node="amount"]')
+   .focus()
+   .blur()
 })
