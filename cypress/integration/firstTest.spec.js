61d4de829275568343a6944dddface54baf5ab71
vitalik991720 <vitalik9917@rambler.ru>
2022-02-14 15:31:10 +0200

    finish lessons 006 web elements

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -69,9 +69,76 @@ it('focus',()=>{
    .focus()
 })
 //blur
-it.only('blur',()=>{
+it('blur',()=>{
    cy.visit('https://next.privat24.ua/mobile?lang=en')
    .get('[data-qa-node="amount"]')
-   .focus()
-   .blur()
+    .type(999)
+    .wait(2000)
+   .clear()
+})
+
+it('submit',()=>{
+   cy.visit('https://next.privat24.ua/mobile?lang=en')
+   .get('form[method="post"]')
+   .submit()
+})
+
+it('click',()=>{
+   cy.visit('https://next.privat24.ua/mobile?lang=en')
+   .get('[data-qa-value="manual"]')
+   .click()
+})
+it('rightclick',()=>{
+   cy.visit('https://example.cypress.io/commands/actions')
+   .contains('Right click to edit')
+   .rightclick()
+})
+it('rightclick',()=>{
+   cy.visit('https://www.mozilla.org/uk/firefox/developer/')
+   .contains('Right click to edit')
+   .dbclick()
+})
+
+it('check',()=>{
+   cy.visit('https://www.facebook.com/reg/?')
+   .get('input[value="2"]')
+   .check()
+})
+
+it('uncheck',()=>{
+   cy.visit('https://en.privatbank.ua/')
+   .get('#switch-input')
+   .check({force: true})
+   .wait(2000)
+   .uncheck({force: true})
+})
+
+it('uncheck',()=>{
+   cy.visit('')
+   .get('#month')
+   .select('Feb')
+   .get('#day')
+   .select('12')
+   .get('#year')
+   .select('1991')
+})
+
+it('check',()=>{
+   cy.visit('https://next.privat24.ua/mobile?lang=en')
+   .get('[data-qa-node="lang"]')
+   .wait(2000)
+   .scrollIntoView()
+})
+
+it('check',()=>{
+   cy.visit('https://next.privat24.ua/mobile?lang=en')
+   .get('[data-qa-node="lang"]')
+   cy.scrollTo(0, 500)
+})
+
+it.only('check',()=>{
+   cy.visit('https://next.privat24.ua/mobile?lang=en')
+   .contains('Services')
+   .wait(2000)
+   .trigger('mouseover')
 })
