ca0f49a60a5974e17c7da3e35eb36d2a6b7d4791
vitalik991720 <vitalik9917@rambler.ru>
2022-02-15 11:02:12 +0200

    finish lessons code

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -12,134 +12,31 @@
 //});
 
 /// <reference types="Cypress" />
-
-// it('SHOULD', ()=>{
-//    cy.visit('https://next.privat24.ua/mobile?lang=en')
-//    cy.get('[data-qa-node="amount"]')
-//    .type(100)
-//    .should('have.value', 100)
-//    .and('be.visible')
-// })
-
-// it('EXPECT', ()=>{
-//    cy.visit('https://next.privat24.ua/mobile?lang=en')
-//    cy.get('[data-qa-node="amount"]')
-//    .type(100).then( input=>{
-//       expect(input).to.have.value(100)
-//    })
-// })
-// it('сheck default value for Deposits', ()=>{
-//    cy.visit('https://next.privat24.ua/deposit?lang=en')
-//    cy.get('[data-qa-value="UAH"]')
-//    .should('be.checked')
-// })
-
-// it('сheck is visible Archive ', ()=>{
-//    cy.visit('https://next.privat24.ua/deposit?lang=en')
-//    cy.contains('Мої депозити')
-//    .trigger('mouseover')
-//    .get('#archiveDeposits')
-//    .should('be.visible') 
-// })
+it('Replenishment of Ukraine mobile phone number', ()=>{
+cy.visit('https://next.privat24.ua/mobile?lang=en')
+.get('[data-qa-node="phone-number"]')
+.type('686979712')
+.get('[data-qa-node="amount"]')
+.type('1')
+.get('[data-qa-node="numberdebitSource"]')
+.type('4552331448138217')
+.get('[data-qa-node="expiredebitSource"]')
+.type('0524')
+.get('[data-qa-node="cvvdebitSource"]')
+.type('111')
+.get('[data-qa-node="submit"]')
+.click()
+//next step check valid card and ect.
+.get('[data-qa-node="card"]')
+.should('have.text', '4552 **** **** 8217')
+.get('[data-qa-node="amount"]')
+.should('have.text', '1')
+.get('[data-qa-node="currency"]')
+.should('have.text', 'UAH')
+.get('[data-qa-node="commission"]')
+.should('have.text', '2')
+.get('[data-qa-node="commission-currency"]')
+.should('have.text', 'UAH')
 
 
-// it('chech is correct attr in button', ()=>{
-//    cy.visit('https://next.privat24.ua?lang=en')
-//    cy.contains('Show cards')
-//    .should('have.attr', 'type') 
-//    .and('match', /button/)
-// })
-// it.only('сheck default value for Deposits', ()=>{
-//    cy.visit('https://next.privat24.ua?lang=en')
-//    cy.url()
-//    .should('eq', 'https://next.privat24.ua/?lang=en')
-// })
-
-
-//type
-it('type',()=>{
-   cy.visit('https://next.privat24.ua/mobile?lang=en')
-   .get('[data-qa-node="phone-number"]')
-   .type(123414 )
-})
-//focus
-it('focus',()=>{
-   cy.visit('https://next.privat24.ua/mobile?lang=en')
-   .get('[data-qa-node="amount"]')
-   .focus()
-})
-//blur
-it('blur',()=>{
-   cy.visit('https://next.privat24.ua/mobile?lang=en')
-   .get('[data-qa-node="amount"]')
-    .type(999)
-    .wait(2000)
-   .clear()
-})
-
-it('submit',()=>{
-   cy.visit('https://next.privat24.ua/mobile?lang=en')
-   .get('form[method="post"]')
-   .submit()
-})
-
-it('click',()=>{
-   cy.visit('https://next.privat24.ua/mobile?lang=en')
-   .get('[data-qa-value="manual"]')
-   .click()
-})
-it('rightclick',()=>{
-   cy.visit('https://example.cypress.io/commands/actions')
-   .contains('Right click to edit')
-   .rightclick()
-})
-it('rightclick',()=>{
-   cy.visit('https://www.mozilla.org/uk/firefox/developer/')
-   .contains('Right click to edit')
-   .dbclick()
-})
-
-it('check',()=>{
-   cy.visit('https://www.facebook.com/reg/?')
-   .get('input[value="2"]')
-   .check()
-})
-
-it('uncheck',()=>{
-   cy.visit('https://en.privatbank.ua/')
-   .get('#switch-input')
-   .check({force: true})
-   .wait(2000)
-   .uncheck({force: true})
-})
-
-it('uncheck',()=>{
-   cy.visit('')
-   .get('#month')
-   .select('Feb')
-   .get('#day')
-   .select('12')
-   .get('#year')
-   .select('1991')
-})
-
-it('check',()=>{
-   cy.visit('https://next.privat24.ua/mobile?lang=en')
-   .get('[data-qa-node="lang"]')
-   .wait(2000)
-   .scrollIntoView()
-})
-
-it('check',()=>{
-   cy.visit('https://next.privat24.ua/mobile?lang=en')
-   .get('[data-qa-node="lang"]')
-   cy.scrollTo(0, 500)
-})
-
-
-it.only('check',()=>{
-   cy.visit('https://next.privat24.ua/mobile?lang=en')
-   .contains('Services')
-   .wait(2000)
-   .trigger('mouseover')
 })
